b4eb0e222cd990feb029787a03e822ec36da8843
TamNhiDuong <yanxinyee@gmail.com>
2025-09-19 00:03:31 +0300

    Get and cache data

--- server/app.js
+++ server/app.js
@@ -1,12 +1,42 @@
 import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
+import postgres from "postgres";
+import { cache } from "@hono/hono/cache";
 
 const app = new Hono();
+const sql = postgres();
 
 app.use("/*", cors());
 app.use("/*", logger());
 
 app.get("/", (c) => c.json({ message: "Hello world!" }));
+
+// get & cache languages
+app.get(
+    "/api/languages",
+    cache({
+        cacheName: "languages-cache",
+        wait: true,
+    }),
+    async (c) => {
+        const languages = await sql`SELECT * FROM languages`;
+        return c.json(languages);
+    }
+);
+
+// get & cache exercises
+app.get(
+    "/api/languages/:id/exercises",
+    cache({
+        cacheName: "exercises-cache",
+        wait: true,
+    }),
+    async (c) => {
+        const langId = c.req.param("id");
+        const exercises = await sql`SELECT id, title, description FROM exercises WHERE language_id = ${langId}`;
+        return c.json(exercises);
+    }
+);
 
 export default app;
