eb89dc86e50baf3d2da01f2b242c1383bd5691c7
TamNhiDuong <yanxinyee@gmail.com>
2025-10-27 14:44:23 +0200

    Astro routes and rendering

---
+++ client/src/components/Exercise.svelte
@@ -0,0 +1,23 @@
+<script>
+  let text = $state("");
+  let submitted = $state(false);
+  let charCount = $state(0);
+  let ifCount = $state(0);
+
+  function handleSubmit() {
+    console.log("CLICK!!!!!!!");
+    submitted = true;
+    charCount = text.length;
+    // Count "if" occurrences (case sensitive)
+    ifCount = (text.match(/\bif\b/g) || []).length;
+  }
+</script>
+
+<textarea bind:value={text}></textarea>
+<br />
+<button on:click={handleSubmit}>Submit</button>
+
+{#if submitted}
+  <p>Characters: {charCount}</p>
+  <p>ifs: {ifCount}</p>
+{/if}
